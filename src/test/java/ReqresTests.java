997a7078b4235d38128b30b7e1bfcad9fc54170a
GorchArtem <rick-flat@mail.ru>
2022-01-02 15:46:11 +0300

    add loginSuccessfulTest

--- src/test/java/ReqresTests.java
+++ src/test/java/ReqresTests.java
@@ -1,3 +1,25 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
 public class ReqresTests extends TestBase {
 
+    @Test
+    void loginSuccessfulTest() {
+        //https://reqres.in/api/login
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+
+    }
 }
