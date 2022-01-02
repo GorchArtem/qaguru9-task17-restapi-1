296d7de6ee0d0898ef3b6b8bbd13cdd5ded4ad36
GorchArtem <rick-flat@mail.ru>
2022-01-02 15:51:58 +0300

    add loginNegativeTest

--- src/test/java/ReqresTests.java
+++ src/test/java/ReqresTests.java
@@ -10,7 +10,7 @@ public class ReqresTests extends TestBase {
     @Test
     void loginSuccessfulTest() {
         //https://reqres.in/api/login
-        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+        String data = "{\"email\":\"eve.holt@reqres.in\",\"password\":\"cityslicka\"}";
 
         given()
                 .contentType(JSON)
@@ -20,6 +20,22 @@ public class ReqresTests extends TestBase {
                 .then()
                 .statusCode(200)
                 .body("token", is("QpwL5tke4Pnpja7X4"));
+
+    }
+
+    @Test
+    void loginNegativeTest() {
+        //https://reqres.in/api/login
+        String data = "{\"email\":\"eve.holt@reqres.in\"}";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
 
     }
 }
